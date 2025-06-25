6db02ea634e3a8b486e88cd81b24034dbda4f25a
raxyPro <52095366+raxyPro@users.noreply.github.com>
2025-06-25 09:45:16 +0530

    1

--- app.old/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-# my_flask_app/auth/__init__.py
-from flask import Blueprint
-
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-from . import routes # Import routes to register them with the blueprint
--- app.old/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-# my_flask_app/tasks/__init__.py
-from flask import Blueprint
-
-tasks_bp = Blueprint('tasks', __name__)
-
-from . import routes # Import routes to register them with the blueprint
